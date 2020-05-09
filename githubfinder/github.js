4f4e49d0e06aab181157b9bc0200e21acfce7397
Abhinav Singh <msabhinavsingh@gmail.com>
2020-05-09 17:33:35 +0530

    Add files via upload

---
+++ githubfinder/github.js
@@ -0,0 +1,26 @@
+class GitHub {
+  constructor() {
+    this.client_id = '96a2af7b2e6394765222';
+    this.client_secret = 'a637d1b7b92a18015917f2d7c79e5fb128f14b86';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
